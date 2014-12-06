fe3ac1ac24d5096a94733ac6a028623323d404f2
Diogo Gonçalves <diogomachadogoncalves@gmail.com>
2014-12-06 19:18:28 -0200

    the route is calculated perfectly

--- caixeiro.cpp
+++ caixeiro.cpp
@@ -4,56 +4,92 @@
 #include "grafo.hpp"
 using namespace std;
 
-//cpc = custo parcial do caminho
 int resultado = INF;
 list<int> caminho;
 
-void imprimeCaminho(vertice *vertices, int v_atual){
-    if(v_atual){
-        imprimeCaminho(vertices, vertices[v_atual].pai);
-        cout << " " << v_atual;
-    }
-    else{
-        cout << "0";
-    }
+/*
+@input:
+    lista de adjacencia do grafo
+*/
+void imprimeCaminho(vertice *vertices){
+
+    list<int>::iterator it;
+
+    for (it=caminho.begin(); it!=caminho.end(); ++it)
+        cout << *it << ' ';
+    cout << '0' << endl;
 }
 
+/*
+adiciona na lista 'caminho' os vertices que compoe o menor caminho
+a insercao é feita do ultimo vertice do caminho(ignorando a volta para a origem)
+para o começo. O lista porem fica organizada da forma correta.
+
+@input:
+    lista de adjacencia do grafo;
+    ultimo vertice adicionado ao caminho
+*/
+void salvaCaminho(vertice *vertices, int v_atual){
+
+    caminho.clear();//exclui caminho anterior
+    while(v_atual){ //vertice nao é a origem
+        caminho.push_front(v_atual);//adiciona no comeco da lista
+        v_atual = vertices[v_atual].pai;//atualiza o vertice
+    }
+    caminho.push_front(0);//adiciona o vertice inicial
+}
+
+/*
+@input:
+    lista de adjacencia do grafo;
+    lista de vertices nao adicionados ao caminho;
+    cpc = custo parcial do caminho;
+    ultimo vertice adicionado ao caminho
+*/
 void backtrack(vertice *vertices, list<int> l, int cpc, int v_atual){
 
-    //cout << "cpc " << cpc << " " << v_atual << endl;
-    if(l.empty()){                 //percorremos todos os vertices
+    //lista vazia == nenhum vertice para se adicionar ao caminho
+    if(l.empty()){
+
         //exite a aresta para a origem?
+        //custo 0 == aresta nao existe
         int custo_aresta = verificaAresta(vertices, v_atual, 0);
-        //cout << "custo_aresta " << custo_aresta << endl;
         if(custo_aresta){           //se existe
-        //    cout << "custo " << cpc + custo_aresta << endl;
+
+            //novo caminho possui um custo melhor que o atual?
             if((cpc + custo_aresta) < resultado){
+                //atualiza o custo final
                 resultado = cpc + custo_aresta;
+                //atualiza o caminho
                 vertices[0].pai = v_atual;
-                //listaVertices(vertices, 3);
-                //salvaCaminho();
-                cout << "custo total " << cpc + custo_aresta << endl;
-                imprimeCaminho(vertices, v_atual);
-                cout << " 0" << endl;
+                salvaCaminho(vertices, v_atual);
             }
         }
     }
-    else{
-        aresta *a = vertices[v_atual].adjacente;
-        while(a){
+    else{//ha vertices para se adicionar ao caminho
+        aresta *a = vertices[v_atual].adjacente;//lista de arestas
+        while(a){//verifica todas as arestas
+
+            /*verifica se a aresta nao aponta para um
+            vertice ja adicionado no caminho*/
             if(vertices[a->dest].pai == -1){
-                if(a->dest){
-                    if((cpc + a->custo) < resultado){
+                if(a->dest){//aresta ainda nao pode apontar para a origem
+                    /*branch and bound
+                    ainda é viavel proceguir no caminho?*/
+                    if((cpc + a->custo) < resultado){//se sim
+                        //remove o vertice da lista de vertices nao adicionados
                         l.remove(a->dest);
-                        vertices[a->dest].pai = v_atual;
-                        //listaVertices(vertices, 5);
+                        vertices[a->dest].pai = v_atual;//atualiza referencias
+                        //explora caminho a partir de a->dest
                         backtrack(vertices, l, cpc+a->custo, a->dest);
+                        /* caminho por a->dest ja foi explorado
+                        reinserir o vertice na lista*/
                         vertices[a->dest].pai = -1;
                         l.push_back(a->dest);
                     }
                 }
             }
-            a = a->prox;
+            a = a->prox;//proxima resta
         }
     }
 
@@ -70,11 +106,16 @@ int main(){
         cin >> ori >> dest >> custo;
         insereAresta(vertices, ori, dest, custo);
     }
+
+    //adiciona todos os vertices à lista, exceto a origem
     for(int i = 1; i < n_ver; i++){
         l.push_back(i);
     }
 
+    //o caminho inicializa-se com custo 0, no vertice 0
     backtrack(vertices, l, 0, 0);
+
+    imprimeCaminho(vertices);
 
     liberaGrafo(vertices, n_ver);
     return 0;
